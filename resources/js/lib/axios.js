5d1f5dda85505fbffb1c3b0097a57dd854c96282
CarlosMateoM <carlosmateo484@gmail.com>
2024-10-02 21:05:54 -0500

    change: url api

--- resources/js/lib/axios.js
+++ resources/js/lib/axios.js
@@ -3,7 +3,7 @@ import router from '../router';
 
 
 const axios = Axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://mateodevelopment.com/api/v1',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
